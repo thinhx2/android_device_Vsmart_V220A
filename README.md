d12357874639c861a1fc4d2f4cb13e5f84a6673d
thinhx2 <nthinh1102@gmail.com>
2020-03-18 21:24:02 +0700

    intial bring up

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# android_device_Vsmart_V220A
+Device configuration for Vsmart Bee
+====================================
+
+Vsmart Bee (codenamed _"V220A"_) is a low-end smartphone from Vsmart.	
+ 
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+OS	| Android 8.1 Go edition
+CPU     | Quad-core 1.5 GHz Cortex-A53
+Chipset | Mediatek MT6739WW (28 nm)
+GPU     | PowerVR GE8100
+Memory  | 1GB RAM
+Storage | 16GB
+MicroSD | up to 128 GB (dedicated slot)
+Battery | Removable Li-Po 2500 mAh battery
+Resolution | 720 x 1440 pixels, 18:9 ratio (~295 ppi density)
+Camera (Rear)  | 8 MP	
+Camera (Front)  | 5 MP
+
+## Device picture
+
+![Vsmart](https://cdn.tgdd.vn/Products/Images/42/207847/vsmart-bee-blue-400x460.png "V220A")
+
---
+++ lineage_V220A.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from Vsmart Bee device
+$(call inherit-product, device/Vsmart/V220A/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_V220A
+PRODUCT_DEVICE := V220A
+PRODUCT_BRAND := Vsmart
+PRODUCT_MODEL := Vsmart Bee
+PRODUCT_MANUFACTURER := Vsmart
+
+# Product characteristics
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1440
+TARGET_SCREEN_WIDTH := 720
+
+# Build info
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vndk.version=27 \
+    ro.skia.ignore_swizzle=true \
+    ro.config.avoid_gfx_accel=true \
+    ro.radio.apn_force_cognitive=true
+
+# GMS
+PRODUCT_GMS_CLIENTID_BASE := android-vsmart
